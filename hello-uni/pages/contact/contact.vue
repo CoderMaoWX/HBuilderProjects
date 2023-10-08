cb633671c3e35e5e57d5eae7fc909ea0f832786d
maowangxin <maowangxin@zaful.com>
2023-10-08 18:11:06 +0800

    hello-uni-app

---
+++ hello-uni/pages/contact/contact.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="our">
+		联系我们
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ hello-uni/pages/message/message.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="he">
+		hello-uni
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
